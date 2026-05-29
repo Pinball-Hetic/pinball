f4b7ec5ef4e90821e2f9202955523d8a1c0580b0
pacardflorian <pacardflo02@gmail.com>
2026-05-29 10:26:51 +0200

    feat: constantes zone sortie sous les flippers

--- packages/game-engine/src/domain/PlayfieldGeometry.ts
+++ packages/game-engine/src/domain/PlayfieldGeometry.ts
@@ -1,4 +1,5 @@
-import { WALL_BOTTOM_Z, BALL_RADIUS } from './Ball';
+import { WALL_BOTTOM_Z, BALL_RADIUS, BALL_SPAWN_POSITION } from './Ball';
+import { FLIPPER_Z_MAX } from './FlipperConstants';
 
 export function surfaceYAtZ(z: number): number {
   return 1.068 - ((z + 0.552) / 0.970) * 0.110;
@@ -10,6 +11,13 @@ export function ballCenterOnSurface(z: number, margin = 0.002): number {
 }
 
 export const DRAIN_Z_THRESHOLD = WALL_BOTTOM_Z + BALL_RADIUS * 2;
+
+export const BOTTOM_OUT_Z = FLIPPER_Z_MAX + 0.025;
+export const BOTTOM_OUT_LANE_SEP_X = BALL_SPAWN_POSITION.x - BALL_RADIUS * 2;
+
+export function isInBottomOutZone(x: number, z: number): boolean {
+  return z >= BOTTOM_OUT_Z && x <= BOTTOM_OUT_LANE_SEP_X;
+}
 
 export const PLAYFIELD_SHADE_W = 0.58;
 export const PLAYFIELD_SHADE_D = 1.02;
