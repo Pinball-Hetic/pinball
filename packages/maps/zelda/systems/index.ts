3bf5ced60e115fc7bf52f5f21eb5f44740b127c2
Imrane Mesbah <imrane.mesbahi@jai20enmaths.com>
2026-06-16 02:47:56 +0200

    feat(maps): create index file for Zelda systems to export key classes and types

---
+++ packages/maps/zelda/systems/index.ts
@@ -0,0 +1,5 @@
+export { BossRevealOrchestrator } from './BossRevealOrchestrator';
+export type { BossRevealController } from './BossRevealController';
+export { GanondorfReveal } from './GanondorfReveal';
+export { GanondorfTargetVisual } from './GanondorfTargetVisual';
+export { PlayfieldCinematicStrobe } from './PlayfieldCinematicStrobe';
